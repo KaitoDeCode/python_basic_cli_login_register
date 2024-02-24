759a83e6b58a821b0ca4b024fe19f01ace84c908
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-25 00:04:16 +0700

    percabangan

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ print("K  K    A   A     I      T      O   O ")
 print("K   K   A   A    III     T       OOO ")
 print("")
 print("========================================")
+
+nomor = 11;
+
+if nomor == 10:
+    print(nomor)
+else:
+    print("Tidak Sesuai")
