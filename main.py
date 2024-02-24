3221f0266da3e910fed9fbb79f57dcf616e90b8c
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-25 00:17:14 +0700

    register

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ pilihan = ''
 kesempatan_login=0;
 kesempatan_register=0;
 
+nama=''
+email=''
+password=''
+
 while pilihan != '3':
     
     pilihan = input('''
@@ -28,7 +32,12 @@ while pilihan != '3':
     choose your option: ''')
     
     if pilihan == '1':
-        print("Anda memilih register")
+        print("REGISTER PROGRAM")
+        nama = input("Masukan nama anda: ")
+        email = input("Masukan email anda: ")
+        password = input("Masukan password anda: ")
+        
+        print(nama,email,password)
         
     elif pilihan == '2':
         print("Anda memilih login")
